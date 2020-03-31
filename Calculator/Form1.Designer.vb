472ff7f3aad82db5d2f9addfc68268ad3062f397
promutuma <44328078+promutuma@users.noreply.github.com>
2020-03-31 11:53:10 +0300

    Add files via upload

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,326 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmd0 = New System.Windows.Forms.Button()
+        Me.cmd1 = New System.Windows.Forms.Button()
+        Me.cmd2 = New System.Windows.Forms.Button()
+        Me.cmd3 = New System.Windows.Forms.Button()
+        Me.cmd4 = New System.Windows.Forms.Button()
+        Me.cmd5 = New System.Windows.Forms.Button()
+        Me.cmd6 = New System.Windows.Forms.Button()
+        Me.cmd7 = New System.Windows.Forms.Button()
+        Me.cmd8 = New System.Windows.Forms.Button()
+        Me.cmd9 = New System.Windows.Forms.Button()
+        Me.cmdEqual = New System.Windows.Forms.Button()
+        Me.cmdClear = New System.Windows.Forms.Button()
+        Me.cmdSubtract = New System.Windows.Forms.Button()
+        Me.cmdMultiply = New System.Windows.Forms.Button()
+        Me.cmdDivide = New System.Windows.Forms.Button()
+        Me.cmdSquare = New System.Windows.Forms.Button()
+        Me.cmdSqrt = New System.Windows.Forms.Button()
+        Me.cmdCos = New System.Windows.Forms.Button()
+        Me.cmdSin = New System.Windows.Forms.Button()
+        Me.cmdTan = New System.Windows.Forms.Button()
+        Me.cmdPlus = New System.Windows.Forms.Button()
+        Me.txtBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'cmd0
+        '
+        Me.cmd0.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmd0.Location = New System.Drawing.Point(13, 123)
+        Me.cmd0.Name = "cmd0"
+        Me.cmd0.Size = New System.Drawing.Size(75, 75)
+        Me.cmd0.TabIndex = 0
+        Me.cmd0.Text = "0"
+        Me.cmd0.UseVisualStyleBackColor = True
+        '
+        'cmd1
+        '
+        Me.cmd1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmd1.Location = New System.Drawing.Point(94, 123)
+        Me.cmd1.Name = "cmd1"
+        Me.cmd1.Size = New System.Drawing.Size(75, 75)
+        Me.cmd1.TabIndex = 1
+        Me.cmd1.Text = "1"
+        Me.cmd1.UseVisualStyleBackColor = True
+        '
+        'cmd2
+        '
+        Me.cmd2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmd2.Location = New System.Drawing.Point(175, 123)
+        Me.cmd2.Name = "cmd2"
+        Me.cmd2.Size = New System.Drawing.Size(75, 75)
+        Me.cmd2.TabIndex = 2
+        Me.cmd2.Text = "2"
+        Me.cmd2.UseVisualStyleBackColor = True
+        '
+        'cmd3
+        '
+        Me.cmd3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmd3.Location = New System.Drawing.Point(256, 123)
+        Me.cmd3.Name = "cmd3"
+        Me.cmd3.Size = New System.Drawing.Size(75, 75)
+        Me.cmd3.TabIndex = 3
+        Me.cmd3.Text = "3"
+        Me.cmd3.UseVisualStyleBackColor = True
+        '
+        'cmd4
+        '
+        Me.cmd4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmd4.Location = New System.Drawing.Point(337, 123)
+        Me.cmd4.Name = "cmd4"
+        Me.cmd4.Size = New System.Drawing.Size(75, 75)
+        Me.cmd4.TabIndex = 4
+        Me.cmd4.Text = "4"
+        Me.cmd4.UseVisualStyleBackColor = True
+        '
+        'cmd5
+        '
+        Me.cmd5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmd5.Location = New System.Drawing.Point(13, 204)
+        Me.cmd5.Name = "cmd5"
+        Me.cmd5.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.cmd5.Size = New System.Drawing.Size(75, 75)
+        Me.cmd5.TabIndex = 5
+        Me.cmd5.Text = "5"
+        Me.cmd5.UseVisualStyleBackColor = True
+        '
+        'cmd6
+        '
+        Me.cmd6.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmd6.Location = New System.Drawing.Point(94, 204)
+        Me.cmd6.Name = "cmd6"
+        Me.cmd6.Size = New System.Drawing.Size(75, 75)
+        Me.cmd6.TabIndex = 6
+        Me.cmd6.Text = "6"
+        Me.cmd6.UseVisualStyleBackColor = True
+        '
+        'cmd7
+        '
+        Me.cmd7.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmd7.Location = New System.Drawing.Point(175, 204)
+        Me.cmd7.Name = "cmd7"
+        Me.cmd7.Size = New System.Drawing.Size(75, 75)
+        Me.cmd7.TabIndex = 7
+        Me.cmd7.Text = "7"
+        Me.cmd7.UseVisualStyleBackColor = True
+        '
+        'cmd8
+        '
+        Me.cmd8.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmd8.Location = New System.Drawing.Point(256, 204)
+        Me.cmd8.Name = "cmd8"
+        Me.cmd8.Size = New System.Drawing.Size(75, 75)
+        Me.cmd8.TabIndex = 8
+        Me.cmd8.Text = "8"
+        Me.cmd8.UseVisualStyleBackColor = True
+        '
+        'cmd9
+        '
+        Me.cmd9.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmd9.Location = New System.Drawing.Point(337, 204)
+        Me.cmd9.Name = "cmd9"
+        Me.cmd9.Size = New System.Drawing.Size(75, 75)
+        Me.cmd9.TabIndex = 9
+        Me.cmd9.Text = "9"
+        Me.cmd9.UseVisualStyleBackColor = True
+        '
+        'cmdEqual
+        '
+        Me.cmdEqual.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdEqual.Location = New System.Drawing.Point(418, 123)
+        Me.cmdEqual.Name = "cmdEqual"
+        Me.cmdEqual.Size = New System.Drawing.Size(75, 75)
+        Me.cmdEqual.TabIndex = 10
+        Me.cmdEqual.Text = "="
+        Me.cmdEqual.UseVisualStyleBackColor = True
+        '
+        'cmdClear
+        '
+        Me.cmdClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdClear.Location = New System.Drawing.Point(499, 123)
+        Me.cmdClear.Name = "cmdClear"
+        Me.cmdClear.Size = New System.Drawing.Size(75, 75)
+        Me.cmdClear.TabIndex = 11
+        Me.cmdClear.Text = "C"
+        Me.cmdClear.UseVisualStyleBackColor = True
+        '
+        'cmdSubtract
+        '
+        Me.cmdSubtract.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdSubtract.Location = New System.Drawing.Point(418, 285)
+        Me.cmdSubtract.Name = "cmdSubtract"
+        Me.cmdSubtract.Size = New System.Drawing.Size(75, 75)
+        Me.cmdSubtract.TabIndex = 12
+        Me.cmdSubtract.Text = "-"
+        Me.cmdSubtract.UseVisualStyleBackColor = True
+        '
+        'cmdMultiply
+        '
+        Me.cmdMultiply.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdMultiply.Location = New System.Drawing.Point(499, 204)
+        Me.cmdMultiply.Name = "cmdMultiply"
+        Me.cmdMultiply.Size = New System.Drawing.Size(75, 75)
+        Me.cmdMultiply.TabIndex = 13
+        Me.cmdMultiply.Text = "*"
+        Me.cmdMultiply.UseVisualStyleBackColor = True
+        '
+        'cmdDivide
+        '
+        Me.cmdDivide.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdDivide.Location = New System.Drawing.Point(499, 285)
+        Me.cmdDivide.Name = "cmdDivide"
+        Me.cmdDivide.Size = New System.Drawing.Size(75, 75)
+        Me.cmdDivide.TabIndex = 14
+        Me.cmdDivide.Text = "/"
+        Me.cmdDivide.UseVisualStyleBackColor = True
+        '
+        'cmdSquare
+        '
+        Me.cmdSquare.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdSquare.Location = New System.Drawing.Point(13, 285)
+        Me.cmdSquare.Name = "cmdSquare"
+        Me.cmdSquare.Size = New System.Drawing.Size(75, 75)
+        Me.cmdSquare.TabIndex = 15
+        Me.cmdSquare.Text = "x2"
+        Me.cmdSquare.UseVisualStyleBackColor = True
+        '
+        'cmdSqrt
+        '
+        Me.cmdSqrt.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdSqrt.Location = New System.Drawing.Point(94, 285)
+        Me.cmdSqrt.Name = "cmdSqrt"
+        Me.cmdSqrt.Size = New System.Drawing.Size(75, 75)
+        Me.cmdSqrt.TabIndex = 16
+        Me.cmdSqrt.Text = "sqrt"
+        Me.cmdSqrt.UseVisualStyleBackColor = True
+        '
+        'cmdCos
+        '
+        Me.cmdCos.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdCos.Location = New System.Drawing.Point(175, 285)
+        Me.cmdCos.Name = "cmdCos"
+        Me.cmdCos.Size = New System.Drawing.Size(75, 75)
+        Me.cmdCos.TabIndex = 17
+        Me.cmdCos.Text = "cos"
+        Me.cmdCos.UseVisualStyleBackColor = True
+        '
+        'cmdSin
+        '
+        Me.cmdSin.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdSin.Location = New System.Drawing.Point(256, 285)
+        Me.cmdSin.Name = "cmdSin"
+        Me.cmdSin.Size = New System.Drawing.Size(75, 75)
+        Me.cmdSin.TabIndex = 18
+        Me.cmdSin.Text = "sin"
+        Me.cmdSin.UseVisualStyleBackColor = True
+        '
+        'cmdTan
+        '
+        Me.cmdTan.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdTan.Location = New System.Drawing.Point(337, 285)
+        Me.cmdTan.Name = "cmdTan"
+        Me.cmdTan.Size = New System.Drawing.Size(75, 75)
+        Me.cmdTan.TabIndex = 19
+        Me.cmdTan.Text = "tan"
+        Me.cmdTan.UseVisualStyleBackColor = True
+        '
+        'cmdPlus
+        '
+        Me.cmdPlus.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdPlus.Location = New System.Drawing.Point(418, 204)
+        Me.cmdPlus.Name = "cmdPlus"
+        Me.cmdPlus.Size = New System.Drawing.Size(75, 75)
+        Me.cmdPlus.TabIndex = 20
+        Me.cmdPlus.Text = "+"
+        Me.cmdPlus.UseVisualStyleBackColor = True
+        '
+        'txtBox
+        '
+        Me.txtBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBox.Location = New System.Drawing.Point(13, 28)
+        Me.txtBox.MaximumSize = New System.Drawing.Size(1000, 80)
+        Me.txtBox.MinimumSize = New System.Drawing.Size(500, 60)
+        Me.txtBox.Name = "txtBox"
+        Me.txtBox.Size = New System.Drawing.Size(562, 80)
+        Me.txtBox.TabIndex = 21
+        '
+        'frmCalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(583, 423)
+        Me.Controls.Add(Me.txtBox)
+        Me.Controls.Add(Me.cmdPlus)
+        Me.Controls.Add(Me.cmdTan)
+        Me.Controls.Add(Me.cmdSin)
+        Me.Controls.Add(Me.cmdCos)
+        Me.Controls.Add(Me.cmdSqrt)
+        Me.Controls.Add(Me.cmdSquare)
+        Me.Controls.Add(Me.cmdDivide)
+        Me.Controls.Add(Me.cmdMultiply)
+        Me.Controls.Add(Me.cmdSubtract)
+        Me.Controls.Add(Me.cmdClear)
+        Me.Controls.Add(Me.cmdEqual)
+        Me.Controls.Add(Me.cmd9)
+        Me.Controls.Add(Me.cmd8)
+        Me.Controls.Add(Me.cmd7)
+        Me.Controls.Add(Me.cmd6)
+        Me.Controls.Add(Me.cmd5)
+        Me.Controls.Add(Me.cmd4)
+        Me.Controls.Add(Me.cmd3)
+        Me.Controls.Add(Me.cmd2)
+        Me.Controls.Add(Me.cmd1)
+        Me.Controls.Add(Me.cmd0)
+        Me.Name = "frmCalc"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmd0 As Button
+    Friend WithEvents cmd1 As Button
+    Friend WithEvents cmd2 As Button
+    Friend WithEvents cmd3 As Button
+    Friend WithEvents cmd4 As Button
+    Friend WithEvents cmd5 As Button
+    Friend WithEvents cmd6 As Button
+    Friend WithEvents cmd7 As Button
+    Friend WithEvents cmd8 As Button
+    Friend WithEvents cmd9 As Button
+    Friend WithEvents cmdEqual As Button
+    Friend WithEvents cmdClear As Button
+    Friend WithEvents cmdSubtract As Button
+    Friend WithEvents cmdMultiply As Button
+    Friend WithEvents cmdDivide As Button
+    Friend WithEvents cmdSquare As Button
+    Friend WithEvents cmdSqrt As Button
+    Friend WithEvents cmdCos As Button
+    Friend WithEvents cmdSin As Button
+    Friend WithEvents cmdTan As Button
+    Friend WithEvents cmdPlus As Button
+    Friend WithEvents txtBox As TextBox
+End Class
